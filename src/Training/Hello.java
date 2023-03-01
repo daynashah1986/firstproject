33cf397180a3c69ef148dc1f79560b8befd513c1
Kamlesh Shah <Kamlesh Shah@LAPTOP-JL6INC90>
2023-02-28 18:02:11 -0600

    uploading

---
+++ src/Training/Hello.java
@@ -0,0 +1,15 @@
+package Training;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Java world");
+		System.out.println("hello good morning");
+		System.out.println();
+		System.out.println("Training for Devops");
+		System.out.println("$100usd discount on iPHone 14 pro");
+		System.out.println("hello");
+System.out.println();
+	}
+
+}
